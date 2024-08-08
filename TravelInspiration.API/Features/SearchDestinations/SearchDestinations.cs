1ea05c6686da57d6bdbb019a0310d1bea25d32ae
Furrman <16333282+Furrman@users.noreply.github.com>
2024-08-08 19:56:57 +0100

    Add SearchDestination slice

---
+++ TravelInspiration.API/Features/SearchDestinations/SearchDestinations.cs
@@ -0,0 +1,21 @@
+﻿using TravelInspiration.API.Shared.Networking;
+
+namespace TravelInspiration.API.Features.SearchDestinations;
+
+public static class SearchDestinations
+{
+    public static void AddEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/destinations", async (string? searchFor,
+            ILoggerFactory loggerFactory,
+            IDestinationSearchApiClient destinationSearchApiClient) =>
+        {
+            loggerFactory.CreateLogger("EndpointHandlers").LogInformation("SearchDestinations featured called");
+
+            var result = await destinationSearchApiClient
+                .GetDestinationsAsync(searchFor, null);
+
+            return Results.Ok(result);
+        });
+    }
+}
