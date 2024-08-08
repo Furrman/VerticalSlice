525d3142931fc949ff439c6c1684804d82dc6390
Furrman <16333282+Furrman@users.noreply.github.com>
2024-08-08 20:07:27 +0100

    Add cancellation token to SearchDestinations

--- TravelInspiration.API/Features/SearchDestinations/SearchDestinations.cs
+++ TravelInspiration.API/Features/SearchDestinations/SearchDestinations.cs
@@ -6,14 +6,24 @@ public static class SearchDestinations
 {
     public static void AddEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapGet("api/destinations", async (string? searchFor,
-            ILoggerFactory loggerFactory,
-            IDestinationSearchApiClient destinationSearchApiClient) =>
+        app.MapGet("api/destinations", 
+            async (string? searchFor,
+                ILoggerFactory loggerFactory,
+                IDestinationSearchApiClient destinationSearchApiClient,
+                CancellationToken cancellationToken) =>
         {
-            loggerFactory.CreateLogger("EndpointHandlers").LogInformation("SearchDestinations featured called");
+            loggerFactory.CreateLogger("EndpointHandlers")
+                .LogInformation("SearchDestinations featured called");
 
-            var result = await destinationSearchApiClient
-                .GetDestinationsAsync(searchFor, null);
+            var resultFromApiCall = await destinationSearchApiClient
+                .GetDestinationsAsync(searchFor, cancellationToken);
+
+            var result = resultFromApiCall.Select(x => new
+            {
+                x.Name,
+                x.Description,
+                x.ImageUri
+            });
 
             return Results.Ok(result);
         });
