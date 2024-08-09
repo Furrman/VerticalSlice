bcced89ccf8a911f7cdcbdccc39498bdf5c827c9
Furrman <16333282+Furrman@users.noreply.github.com>
2024-08-09 14:09:17 +0100

    Implement mediator pattern to GetStops endpoint

--- TravelInspiration.API/Features/Stops/GetStops.cs
+++ TravelInspiration.API/Features/Stops/GetStops.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using TravelInspiration.API.Shared.Domain.Entities;
 using TravelInspiration.API.Shared.Persistence;
@@ -12,40 +13,64 @@ public static class GetStops
         app.MapGet("api/itineraries/{itineraryId}/stops", async
             (int itineraryId,
                 ILoggerFactory loggerFactory,
-                IMapper mapper,
-                TravelInspirationDbContext dbContext,
+                IMediator mediator,
                 CancellationToken cancellationToken) =>
     {
             loggerFactory.CreateLogger("EndpointHandlers")
                 .LogInformation("GetStops feature called.");
 
-            var itinerary = await dbContext.Itineraries
+            return await mediator.Send(
+                new GetStopsQuery(itineraryId),
+                cancellationToken);
+        });
+    }
+
+    public class GetStopsQuery(int itineraryId) : IRequest<IResult>
+    {
+        public int ItineraryId { get; } = itineraryId;
+    }
+
+    public class GetStopsQueryHandler : IRequestHandler<GetStopsQuery, IResult>
+    {
+        private readonly TravelInspirationDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetStopsQueryHandler(TravelInspirationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<IResult> Handle(GetStopsQuery request, CancellationToken cancellationToken)
+        {
+            var itinerary = await _dbContext.Itineraries
                 .Include(i => i.Stops)
-                .FirstOrDefaultAsync(i => i.Id == itineraryId, cancellationToken);
+                .FirstOrDefaultAsync(i => i.Id == request.ItineraryId, cancellationToken);
 
             if (itinerary == null)
             {
                 return Results.NotFound();
             }
-            var result = mapper.Map<IEnumerable<StopDto>>(itinerary.Stops);
+            var result = _mapper.Map<IEnumerable<StopDto>>(itinerary.Stops);
 
             return Results.Ok(result);
-        });
+        }
     }
-}
 
-public sealed class StopDto
-{
-    public required int Id { get; set; }
-    public required string Name { get; set; }
-    public Uri? ImageUri { get; set; }
-    public required int ItineraryId { get; set; }
-}
-
-public sealed class StopProfile : Profile
-{
-    public StopProfile()
+    public sealed class StopDto
     {
-        CreateMap<Stop, StopDto>();
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public Uri? ImageUri { get; set; }
+        public required int ItineraryId { get; set; }
     }
+
+    public sealed class StopProfile : Profile
+    {
+        public StopProfile()
+        {
+            CreateMap<Stop, StopDto>();
+        }
+    }
+
 }
