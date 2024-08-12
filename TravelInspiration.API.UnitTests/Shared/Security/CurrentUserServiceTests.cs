3598403b2af3ede361f3af92e6834a1e4673a2f1
Furrman <16333282+Furrman@users.noreply.github.com>
2024-08-12 15:46:28 +0100

    Add CurrentUserServiceTests with UserId test

---
+++ TravelInspiration.API.UnitTests/Shared/Security/CurrentUserServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using System.Security.Claims;
+using TravelInspiration.API.Shared.Security;
+
+namespace TravelInspiration.API.UnitTests.Shared.Security;
+
+public class CurrentUserServiceTests
+{
+    [Fact]
+    public void WhenGettingUser_WithNameIdentifierClaimInContext_NameIdentifierMustBeReturned()
+    {
+        // Arrange
+        var httpContextAccesor = new Mock<IHttpContextAccessor>();
+        var identity = new ClaimsIdentity(
+            [
+                new(ClaimTypes.NameIdentifier, "Kevin")
+            ],
+            "Test",
+            ClaimTypes.NameIdentifier,
+            ClaimTypes.Role);
+        var contextUser = new ClaimsPrincipal(identity);
+        var httpContext = new DefaultHttpContext
+        {
+            User = contextUser
+        };
+        httpContextAccesor.Setup(x => x.HttpContext).Returns(httpContext);
+        var currentUserService = new CurrentUserService(httpContextAccesor.Object);
+
+        // Act
+        var nameIdentifier = currentUserService.UserId;
+
+        // Assert
+        Assert.Equal("Kevin", nameIdentifier);
+    }
+}
