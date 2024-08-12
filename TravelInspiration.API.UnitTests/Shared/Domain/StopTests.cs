8982381de19a55a193ad3c09251c275cb7ddb6b3
Furrman <16333282+Furrman@users.noreply.github.com>
2024-08-12 15:33:21 +0100

    Add IDisposable for StopTests to share some common code

--- TravelInspiration.API.UnitTests/Shared/Domain/StopTests.cs
+++ TravelInspiration.API.UnitTests/Shared/Domain/StopTests.cs
@@ -4,8 +4,19 @@ using static TravelInspiration.API.Features.Stops.CreateStop;
 
 namespace TravelInspiration.API.UnitTests.Shared.Domain;
 
-public class StopTests
+public class StopTests : IDisposable
 {
+    private readonly Stop _stop;
+    private readonly CreateStopCommand _createStopCommand;
+
+    public StopTests()
+    {
+        _stop = new Stop("StopForTesting");
+        _createStopCommand = new CreateStopCommand(42,
+            "A name",
+            null);
+    }
+
     // unitofwork_stateundertest_expectedbehavior
     // given_when_then
 
@@ -13,16 +24,12 @@ public class StopTests
     public void WhenExecutingHandleCreateCommand_WithItineraryId_StopItineraryIdMustMatch()
     {
         // Arrange
-        var stop = new Stop("StopForTesting");
-        var createStopCommand = new CreateStopCommand(42,
-            "A name",
-            null);
 
         // Act
-        stop.HandleCreateCommand(createStopCommand);
+        _stop.HandleCreateCommand(_createStopCommand);
 
         // Assert
-        Assert.Equal(createStopCommand.ItineraryId, stop.ItineraryId);
+        Assert.Equal(_createStopCommand.ItineraryId, _stop.ItineraryId);
     }
 
     [Fact]
@@ -40,5 +47,10 @@ public class StopTests
         // Assert
         Assert.Single(stop.DomainEvents);
         Assert.IsType<StopCreatedEvent>(stop.DomainEvents[0]);
+    }
+
+    public void Dispose()
+    {
+        // No code in here
     }
 }
