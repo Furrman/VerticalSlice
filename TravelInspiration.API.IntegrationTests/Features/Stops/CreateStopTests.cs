5a4375dfb9aece132418790ba228ee8fb1af63ff
Furrman <16333282+Furrman@users.noreply.github.com>
2024-08-12 17:34:13 +0100

    Refactor Integration tests to have more reusable code

--- TravelInspiration.API.IntegrationTests/Features/Stops/CreateStopTests.cs
+++ TravelInspiration.API.IntegrationTests/Features/Stops/CreateStopTests.cs
@@ -1,6 +1,4 @@
-﻿using MediatR;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
 using TravelInspiration.API.Features.Stops;
 using TravelInspiration.API.IntegrationTests.Fixtures;
 using TravelInspiration.API.Shared.Domain.Entities;
@@ -15,33 +13,28 @@ public class CreateStopTests(SliceFixture sliceFixture)
     [Fact]
     public async Task WhenExecutingCreateStopSlice_WithValidInput_StopMustBeCreated()
     {
-        // Arrange
-        using var scope = _sliceFixture.ServiceScopeFactory.CreateScope();
-        var context = _sliceFixture.CreateContext(scope);
-        await context.Database.BeginTransactionAsync();
+        await _sliceFixture.ExecuteInTransactionAsync(async (context) =>
+        {
+            // Arrange
+            var itinerary = new Itinerary("Test", "SomeUserId");
+            context.Add(itinerary);
+            await context.SaveChangesAsync();
 
-        var itinerary = new Itinerary("Test", "SomeUserId");
-        context.Add(itinerary);
-        await context.SaveChangesAsync();
+            var cmd = new CreateStop.CreateStopCommand(itinerary.Id,
+                "A stop for testing",
+                null
+            );
 
-        var cmd = new CreateStop.CreateStopCommand(itinerary.Id,
-            "A stop for testing",
-            null
-        );
-        var mediator = scope.ServiceProvider
-            .GetRequiredService<IMediator>();
+            // Act
+            await _sliceFixture.SendAsync(cmd);
 
-        // Act
-        await mediator.Send(cmd);
-
-        // Assert
-        context.ChangeTracker.Clear();
-        var stop = await context.Stops
-            .FirstOrDefaultAsync(s => s.Name == "A stop for testing");
-        Assert.NotNull(stop);
-        Assert.Equal(cmd.ItineraryId, stop.ItineraryId);
-        Assert.Equal(cmd.Name, stop.Name);
-
-        await context.Database.RollbackTransactionAsync();
+            // Assert
+            context.ChangeTracker.Clear();
+            var stop = await context.Stops
+                .FirstOrDefaultAsync(s => s.Name == "A stop for testing");
+            Assert.NotNull(stop);
+            Assert.Equal(cmd.ItineraryId, stop.ItineraryId);
+            Assert.Equal(cmd.Name, stop.Name);
+        });
     }
 }
--- TravelInspiration.API.IntegrationTests/Fixtures/SliceFixture.cs
+++ TravelInspiration.API.IntegrationTests/Fixtures/SliceFixture.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using TravelInspiration.API.IntegrationTests.Factories;
 using TravelInspiration.API.Shared.Persistence;
 
@@ -6,8 +8,9 @@ namespace TravelInspiration.API.IntegrationTests.Fixtures;
 
 public class SliceFixture
 {
-    public IServiceScopeFactory ServiceScopeFactory { get; }
     private readonly TravelInspirationWebApplicationFactory _factory;
+    private IServiceScopeFactory _serviceScopeFactory;
+    private IServiceScope? _scope;
 
     private static readonly object _lock = new();
     private static bool _databaseInitialized;
@@ -15,14 +18,14 @@ public class SliceFixture
     public SliceFixture()
     {
         _factory = new TravelInspirationWebApplicationFactory();
-        ServiceScopeFactory = _factory.Services
+        _serviceScopeFactory = _factory.Services
             .GetRequiredService<IServiceScopeFactory>();
 
         lock (_lock)
         {
             if (!_databaseInitialized)
             {
-                using var scope = ServiceScopeFactory.CreateScope();
+                using var scope = _serviceScopeFactory.CreateScope();
                 using var context = CreateContext(scope);
                 context.Database.EnsureDeleted();
                 context.Database.EnsureCreated();
@@ -38,5 +41,29 @@ public class SliceFixture
         var context = scope.ServiceProvider
             .GetRequiredService<TravelInspirationDbContext>();
         return context;
+    }
+
+    public async Task ExecuteInTransactionAsync(
+        Func<TravelInspirationDbContext, Task> actionToExecute)
+    {
+        using (_scope = _serviceScopeFactory.CreateScope())
+        {
+            var context = CreateContext(_scope);
+            await context.Database.BeginTransactionAsync();
+            await actionToExecute(context);
+            await context.Database.RollbackTransactionAsync();
+        }
+    }
+
+    public async Task SendAsync(IRequest<IResult> cmdOrQuery)
+    {
+        if (_scope is null)
+        {
+            throw new InvalidOperationException(
+                "You must call ExecuteInTransactionAsync before calling SendAsync");
+        }
+
+        var mediator = _scope.ServiceProvider.GetRequiredService<IMediator>();
+        await mediator.Send(cmdOrQuery);
     }
 }
